e772eec44091fbee0c969cfdc1d6e84611fc201e
kuninet <kuninet@gmail.com>
2026-06-27 13:14:46 +0900

    fix: AppDelegate から未使用の import Combine と cancellables フィールドを削除

--- Sources/Remora/AppDelegate.swift
+++ Sources/Remora/AppDelegate.swift
@@ -1,10 +1,7 @@
 import AppKit
-import Combine
 
 @MainActor
 final class AppDelegate: NSObject, NSApplicationDelegate {
-    private var cancellables: Set<AnyCancellable> = []
-
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.setActivationPolicy(.accessory)
     }
